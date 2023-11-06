0cdc8c07a2e2da34428e0b96c6f9c8795aadcd13
Deo Raj <d.jrajsingh81@gmail.com>
2023-11-07 00:05:51 +0530

    Done with the carousel

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useState } from "react";
+import TrainWithUsData from "./TrainWithUsData";
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
+import './App.css'
+const App = () => {
+  const [slide, setSlide] = useState(0);
+  const prevSlide = () => {
+    slide === 0 ? setSlide(TrainWithUsData.length - 1) : setSlide(slide - 1);
+  };
+  const nextSlide = () => {
+    slide === TrainWithUsData.length - 1 ? setSlide(0) : setSlide(slide + 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="gallery">
+      <div className="gallerycarousel">
+        <BsArrowLeftCircleFill
+          onClick={prevSlide}
+          className="arrow arrow-left"
+        />
+        {TrainWithUsData.map((curElem) => {
+          return (
+            <img
+              key={curElem.id}
+              src={curElem.image}
+              className={slide === curElem.id ? "slide" : "slide slide-hidden"}
+            />
+          );
+        })}
+        <BsArrowRightCircleFill
+          onClick={nextSlide}
+          className="arrow arrow-right"
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/TrainWithUsData.js
@@ -0,0 +1,11 @@
+import image0 from "./assets/slide_img1.png";
+import image1 from "./assets/slide_img2.png";
+import image2 from "./assets/slide_img3.png";
+import image3 from "./assets/slide_img4.png";
+const TrainWithUsData = [
+  { id: 0, image: image0 },
+  { id: 1, image: image1 },
+  { id: 2, image: image2 },
+  { id: 3, image: image3 },
+];
+export default TrainWithUsData;
